c615943c4b30bc208ef0bbb94d81445b24bfebd1
fsmdsohel <fsmdshohelrana@gmail.com>
2021-12-18 02:32:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@cluster0.9ppr5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const run = () => {
+  const products = client.db("accessories").collection("products");
+
+  app.get("/products", async (req, res) => {
+    const cursor = products.find({});
+    const data = await cursor.toArray();
+    res.json(data);
+  });
+};
+client.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    run();
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Tech vision server running 🚀");
+});
+
+app.listen(port, () => {
+  console.log("Server running at port: ", port);
+});
